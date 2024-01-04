fcbc8c4a5ecae8459c4f3ddebfdb935b32592478
sametakalinkod <107934311+sametakalinkod@users.noreply.github.com>
2024-01-05 00:38:14 +0300

    .

--- src/app/modules/admin/home/home.component.ts
+++ src/app/modules/admin/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('cosmosDance_video_player') videoPlayer: ElementRef | undefined;
+  pauseMainPageVid: boolean = true;
   videoMuted: boolean = true;
   constructor() { }
 
@@ -15,6 +17,18 @@ export class HomeComponent implements OnInit {
 
   handleClick(): void {
     this.videoMuted = !this.videoMuted;
+  }
+
+  onHoverMethod(value: boolean): void {
+    this.pauseMainPageVid = value;
+    console.log('Hovered!');
+    var dance_video = document.getElementById("cosmosDance_video_player") as HTMLVideoElement;
+
+    if (dance_video && value) {
+      dance_video.play();
+    } else if (dance_video) {
+      dance_video.pause();
+    }
   }
 
 }
