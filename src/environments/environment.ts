939fa0c7f5799b91b004533f351b9600b23a467d
sametakalinkod <107934311+sametakalinkod@users.noreply.github.com>
2024-03-27 14:08:23 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,15 @@ export const environment = {
     production: false,
 
     elasticSearchEndpoint: 'http://localhost:5010/services/elastic',
-    apiUrl: 'http://localhost:5010/services/',
-    hubUrl: 'http://localhost:5015/serverHub',
+    // apiUrl: 'http://localhost:5010/services/',
+    // hubUrl: 'http://localhost:5015/serverHub',
 
     // apiUrl: 'https://betaapi.veboni.com/services/',
     // hubUrl: 'https://hubbeta.veboni.com/serverHub',
 
 
-    // apiUrl: 'https://testapi.veboni.com/services/',
-    // hubUrl: 'https://hubtest.veboni.com/serverHub',
+    apiUrl: 'https://testapi.veboni.com/services/',
+    hubUrl: 'https://hubtest.veboni.com/serverHub',
 
 
     appName: 'veboni',
